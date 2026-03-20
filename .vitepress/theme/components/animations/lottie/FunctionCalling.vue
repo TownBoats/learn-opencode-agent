b43005adc891b41b8462cd8b345db2c78df97af2
张艳华 <939593210@qq.com>
2026-03-20 16:16:30 +0800

    feat(animation): add FunctionCalling lottie component

---
+++ .vitepress/theme/components/animations/lottie/FunctionCalling.vue
@@ -0,0 +1,30 @@
+<template>
+  <AnimationContainer title="工具调用流程" @restart="handleRestart">
+    <template #default="{ isVisible }">
+      <LottiePlayer
+        ref="playerRef"
+        :animation-data="animationData"
+        :autoplay="isVisible"
+        :loop="false"
+        @complete="handleComplete"
+      />
+    </template>
+  </AnimationContainer>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import AnimationContainer from '../core/AnimationContainer.vue'
+import LottiePlayer from '../core/LottiePlayer.vue'
+import animationData from './assets/function-calling.json'
+
+const playerRef = ref<InstanceType<typeof LottiePlayer> | null>(null)
+
+function handleRestart() {
+  playerRef.value?.restart()
+}
+
+function handleComplete() {
+  console.log('Function calling animation completed')
+}
+</script>
